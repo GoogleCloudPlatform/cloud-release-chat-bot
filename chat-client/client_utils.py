65849be36185d927ac40ae2b3153996b73909381
ddeleo <danieldeleo@google.com>
2025-07-21 23:16:42 -0400

    remove message_reply_option since it causes message delivery failure when no thread key is also specified

--- chat-client/client_utils.py
+++ chat-client/client_utils.py
@@ -142,8 +142,6 @@ def send_chat_message(space_id, message):
             request=google_chat.CreateMessageRequest(
                 parent=space_id,
                 message=message,
-                # Set the message reply option to fallback to a new thread
-                message_reply_option=google_chat.CreateMessageRequest.MessageReplyOption.REPLY_MESSAGE_FALLBACK_TO_NEW_THREAD,
             )
         )
         print(f"Response from sending chat message: {response}")
